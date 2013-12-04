4467ef7813f3c506c930fddfde8dc41bf0c1866f
JimVanG <jjvg@me.com>
2013-12-04 13:09:43 -0500

    Init Commit

---
+++ src/RunLikeHell.java
@@ -0,0 +1,18 @@
+/*
+ * James Van Gaasbeck
+ * PID: J2686979
+ * COP 3503 : Computer Science II
+ * Professor Sean Szumlanski
+ * Assignment 8 : Run Like Hell 
+ * Due: Saturday, December 7th, 11:59pm
+ * 
+ */
+public class RunLikeHell {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
