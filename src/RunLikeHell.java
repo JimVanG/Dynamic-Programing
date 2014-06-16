58666412011d841d0db8dba0d1e86547b2c5a6f6
James <jjvg@knights.ucf.edu>
2014-06-16 12:40:55 -0400

    Update RunLikeHell.java

--- src/RunLikeHell.java
+++ src/RunLikeHell.java
@@ -1,9 +1,7 @@
 /*
- * James Van Gaasbeck
- * PID: J2686979
+ * James V
  * COP 3503 : Computer Science II
- * Professor Sean Szumlanski
- * Assignment 8 : Run Like Hell 
+ * Run Like Hell 
  * Due: Saturday, December 7th, 11:59pm
  * 
  */
